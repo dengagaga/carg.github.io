597761e1eccb11874e6d881e4d4618e803d7ba56
dengagaga <134398955+dengagaga@users.noreply.github.com>
2024-05-24 11:49:25 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// var swiper = new Swiper(".mySwiper22", {
+//     direction: "vertical",
+//     slidesPerView: 1.24,
+//     centeredSlides: true,
+//     spaceBetween: 30,
+//     mousewheel: true,
+//     loop:true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
